d69fcaa9a88c15cabdfd5d7ce802097219ab2a26
Olga Zhidkova <zhidkova.olga12@gmail.com>
2022-01-19 16:06:30 +0300

    Initial Commit

---
+++ AppleMusicSwiftUI/AppleMusicSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicSwiftUIApp.swift
+//  AppleMusicSwiftUI
+//
+//  Created by Ольга on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
