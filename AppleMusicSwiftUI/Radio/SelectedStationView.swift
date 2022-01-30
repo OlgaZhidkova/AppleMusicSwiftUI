57c9262986bf6ff70800db6da77e07790b73a948
Olga Zhidkova <zhidkova.olga12@gmail.com>
2022-01-31 00:42:36 +0300

    Fix bugs.

--- AppleMusicSwiftUI/Radio/SelectedStationView.swift
+++ AppleMusicSwiftUI/Radio/SelectedStationView.swift
@@ -11,41 +11,46 @@ struct SelectedStationView: View {
     @State var stations = selectedStatiions
 
     let rows = [
-        GridItem(.fixed(300))
+        GridItem(.flexible())
     ]
     
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            LazyHGrid(rows: rows) {
-                ForEach(stations, id: \.self) { station in
-                    VStack(spacing: 0) {
-                        Divider()
-                        Spacer()
-                        Text(station.type)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundColor(.secondary)
-                            .font(.caption)
-                        Text(station.name)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .font(.title3)
-                        Text(station.description)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                            .foregroundColor(.secondary)
-                            .font(.title3)
-                        Spacer()
-                        Image(station.image)
-                            .resizable()
-                            .frame(width: 360, height: 240)
-                            .cornerRadius(5)
-                        Spacer()
-                        Divider()
+        GeometryReader { geometry in
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHGrid(rows: rows) {
+                    ForEach(stations, id: \.self) { station in
+                        VStack(spacing: 0) {
+                            Divider()
+                            Spacer()
+                            Text(station.type)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .foregroundColor(.secondary)
+                                .font(.caption)
+                            Text(station.name)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .font(.title3)
+                            Text(station.description)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .foregroundColor(.secondary)
+                                .font(.title3)
+                            Spacer()
+                            Image(station.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: geometry.size.width)
+                                .cornerRadius(5)
+                            Spacer()
+                            Divider()
+                        }
                     }
                 }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
         }
+        .frame(height: UIScreen.main.bounds.height * 0.38)
     }
 }
+
 
 struct StationView_Previews: PreviewProvider {
     static var previews: some View {
